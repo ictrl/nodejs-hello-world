ec24e3eb5484812be20677a84e55716aef34110f
Samrat <saurav.k@harnstech.com>
2022-02-20 05:07:19 +0530

    update msg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function (request, response) {
   response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("greeting Jenkins 😁!!!!");
+  response.end("Oh Yeah!, I did it");
 });
 
 var port = 80;
--- test/test.js
+++ test/test.js
@@ -7,26 +7,6 @@ var server = supertest.agent("http://localhost:80");
 
 // UNIT test begin
 
-<<<<<<< HEAD
-describe("SAMPLE unit test",function(){
-
-  // #1 should return home page
-  it("should return home page",function(done){
-    // calling home page
-    server
-    .get("/")
-    .expect("Content-type",/text/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-      res.status.should.equal(200);
-      done();
-    });
-  });
-
-});
-
-=======
 describe("SAMPLE unit test", function () {
   // #1 should return home page
   it("should return home page", function (done) {
@@ -42,4 +22,3 @@ describe("SAMPLE unit test", function () {
       });
   });
 });
->>>>>>> f4fdb2d510dcead6c8cee251e97860c2c8b5ad1b
