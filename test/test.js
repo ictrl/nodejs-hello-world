9c999ffe23e2b0861d51cd2d76c10402c02c0dd1
Samrat <saurav.k@harnstech.com>
2022-02-21 05:53:51 +0530

    update msg

--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
 var request = require("supertest");
+var should = require("should");
 var app = require("../index.js");
 
 describe("GET /", function () {
-  it("respond with hello world", function (done) {
-    //navigate to root and check the the response is "hello world"
-    request(app).get("/").expect("hello world", done);
+  it("respond with 200 status code", function (done) {
+    //navigate to root and check the the response is "Automation done"
+    // request(app).get("/").expect("Automation done", done);
+    request(app)
+      .get("/")
+      .expect(200) // THis is HTTP response
+      .end(function (err, res) {
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        done();
+      });
   });
 });
